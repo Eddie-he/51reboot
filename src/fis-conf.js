a7ffdf0a2858941a3fbea5cb15a21acc924fdaa9
shengxinjing <316783812@qq.com>
2015-07-03 10:53:19 +0800

    add about page
    add about page

--- src/fis-conf.js
+++ src/fis-conf.js
@@ -6,10 +6,14 @@ fis.config.set('modules.postpackager', 'simple');
 
 // 取消下面的注释设置打包规则
 fis.config.set('pack', {
-    '/pkg/main.js': '**.js',
+    // '/pkg/main.js': '**.js',
     // 取消下面的注释设置CSS打包规则，CSS打包的同时会进行图片合并
-    '/pkg/main.css': '**.css'
+    // '/pkg/main.css': '**.css'
 });
-
+fis.config.set('modules.optimizer.html', 'html-minifier');
 // 取消下面的注释可以开启simple对零散资源的自动合并
 fis.config.set('settings.postpackager.simple.autoCombine', true);
+
+fis.config.set('settings.optimizer.html-minifier', {
+	minifyJS: true
+});
