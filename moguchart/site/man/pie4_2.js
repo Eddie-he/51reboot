5aa0a6f52af37b45f1ded916d028893c5440b429
shengxinjing <316783812@qq.com>
2016-12-16 22:21:35 +0800

    aaaaaaa sagh

---
+++ moguchart/site/man/pie4_2.js
@@ -0,0 +1,77 @@
+!function(){
+     // 基于准备好的dom，初始化echarts实例
+        var myChart = echarts.init(document.getElementById('pie4_2'));
+
+        // 指定图表的配置项和数据
+
+let option = {
+    title:{
+        // text:'助攻能力'
+    },
+    tooltip: {
+        trigger: 'item',
+        formatter: "{a} <br/>{b}: {c} ({d}%)"
+    },
+
+    toolbox: {
+        feature: {
+            dataView: {},
+            saveAsImage: {}
+        }
+    },
+    
+    // legend: {
+    //     // orient: 'vertical',
+    //     x: 'center',
+    //     data:['AST Adj','Potential AST','others','FT AST','Secondary AST','AST']
+    // },
+    //     toolbox:{
+    //                 top:15,
+    //     feature:{
+    //         dataView:{
+    //             show:true
+    //         }
+    //     }
+    // },
+    series: [
+        {
+            name:'加内特助攻分析',
+            type:'pie',
+            selectedMode: 'single',
+            radius: [0, '40%'],
+            label: {
+                normal: {
+                    position: 'inner'
+                }
+            },
+            labelLine: {
+                normal: {
+                    show: false
+                }
+            },
+            data:[
+                {value:9.9, name:'AST Adj'},
+                {value:12.3, name:'Potential AST'},
+                {value:41.9, name:'others'}
+            ]
+        },
+        {
+            name:'加内特助攻分析',
+            type:'pie',
+            radius: ['50%', '75%'],
+
+            data:[
+                {value:6.7, name:'AST'},
+                {value:0.7, name:'FT AST'},
+                {value:2.5, name:'Secondary AST'},
+                {value:12.3, name:'Potential AST'},
+                {value:41.9, name:'others'}
+            ]
+        }
+    ]
+};
+
+
+        // 使用刚指定的配置项和数据显示图表。
+        myChart.setOption(option);
+}()
---
+++ moguchart/site/man/radar4_2.js
@@ -0,0 +1,152 @@
+!function(){
+
+let play_name = 'Curry'
+let data_index = [['命中率','5,8,11,20'],['综合能力',14,15,16,17,18]]
+let label = 'GP  MIN PTS FGM FGA FG% 3PM 3PA 3P% FTM FTA FT% OREB  DREB  REB AST STL BLK TOV EFF eFG%'.split(' ').filter(v=>v)
+let all_data = [
+  '2015-2016:79  34.2  30.1  10.2  20.2  50.4  5.1 11.2  45.4  4.6 5.1 90.8  0.9 4.6 5.4 6.7 2.1 0.2 3.3 30.7 63',
+  '2014-2015:80  32.7  23.8  8.2 16.8  48.7  3.6 8.1 44.3  3.9 4.2 91.4  0.7 3.6 4.3 7.7 2.0 0.2 3.1 25.9 59.4',
+  '2013-2014:78 36.5  24.0  8.4 17.7  47.1  3.3 7.9 42.4  3.9 4.5 88.5  0.6 3.7 4.3 8.5 1.6 0.2 3.8 25.0 56.4',
+  '2012-2013:78 38.2  22.9  8.0 17.8  45.1  3.5 7.7 45.3  3.4 3.7 90.0  0.8 3.3 4.0 6.9 1.6 0.1 3.1 22.4 50.9'
+].map(v=>{
+  let temp = v.split(':')
+  return [temp[0],temp[1].split(' ').filter(v=>v)]
+})
+
+// let 
+// 命中率
+
+// let data_score = data.map(val=>{
+//   console.log(val[0])
+//   console.log(val[1].filter((v,i)=>{
+//    return  _.indexOf('5,8,11,20'.split(','),i)>-1
+//   }))
+// })
+// console.log(data_score)
+        // 基于准备好的dom，初始化echarts实例
+      
+
+        // 指定图表的配置项和数据
+
+
+
+
+
+
+
+
+
+
+
+
+        // 指定图表的配置项和数据
+        var option3 = {
+    title: {
+        show:false,
+        text: '2015-2016 命中率'
+    },
+    //     toolbox:{
+    //                 top:15,
+    //     feature:{
+    //         dataView:{
+    //             show:true
+    //         }
+    //     }
+    // },
+    legend: {
+    },
+    toolbox: {
+        feature: {
+            dataView: {},
+            saveAsImage: {}
+        }
+    },
+    
+    radar: [
+        {
+            indicator: [
+                // { text: '指标一' },
+                // { text: '指标二' },
+                // { text: '指标三' },
+                // { text: '指标四' },
+                // { text: '指标五' }
+            ],
+            center: ['25%', '50%'],
+            radius: 120,
+            startAngle: 90,
+            splitNumber: 4,
+            shape: 'circle',
+            name: {
+                formatter:'【{value}】',
+                textStyle: {
+                    color:'#72ACD1'
+                }
+            },
+            splitArea: {
+                areaStyle: {
+                    color: ['rgba(114, 172, 209, 0.2)',
+                    'rgba(114, 172, 209, 0.4)', 'rgba(114, 172, 209, 0.6)',
+                    'rgba(114, 172, 209, 0.8)', 'rgba(114, 172, 209, 1)'],
+                    shadowColor: 'rgba(0, 0, 0, 0.3)',
+                    shadowBlur: 10
+                }
+            },
+
+        },
+        {
+          // 篮板，助攻，断球，盖帽，失误
+          // REB AST STL BLK TOV
+          // 
+          // 2015-2016: 5.4  6.7 2.1 0.2 3.3
+          // 2014-2015: 4.3  7.7 2.0 0.2 3.1
+          // 2013-2014: 4.3  8.5 1.6 0.2 3.8
+            indicator: [
+                { text: 'FG%', max: 100 },
+                { text: '3P%', max: 100 },
+                { text: 'FT%', max: 100 },
+                { text: 'eFG%', max: 100 },
+            ],
+            radius: 90
+        }
+    ],
+    series: [
+        
+        {
+            name: '湖人队',
+            type: 'radar',
+            radarIndex: 1,
+                        // center: ['75%', '50%'],
+
+            data: [
+                {
+                    value: [40,20,90,50],
+                    name: '平均'
+                },
+                {
+
+                    value: [all_data[3][1][5],all_data[3][1][8],all_data[3][1][11],all_data[3][1][20]],
+                    name: '凯尔特人队',
+                    areaStyle: {
+                        normal: {
+                            opacity: 0.9,
+                            color: new echarts.graphic.RadialGradient(0.5, 0.5, 1, [
+                                {
+                                    color: '#B8D3E4',
+                                    offset: 0
+                                },
+                                {
+                                    color: '#72ACD1',
+                                    offset: 1
+                                }
+                            ])
+                        }
+                    }
+                }
+            ]
+        }
+    ]
+}
+
+echarts.init(document.getElementById('radar4_2')).setOption(option3)
+
+}()
